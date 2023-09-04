001661ab340e82f271d6d817177b755c4dd46e70
Fabian Vallejo <favaro1184@gmail.com>
2023-09-04 12:41:54 -0500

    Version 1.0 - Crear parte1 y parte2

---
+++ parte1.py
@@ -0,0 +1,6 @@
+import datetime as dt
+
+ahora = dt.datetime.now()
+
+print(f"{ahora.day}/{ahora.month}/{ahora.year}")
+print("Fin")
---
+++ parte2.py
@@ -0,0 +1,5 @@
+import math
+
+x = math.sqrt(math.pi)
+
+print("La raiz cuadrada de PI es: ",x)
